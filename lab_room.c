666ff964e239e5c99bd652caba78f27a3fc0270c
wzyzsy <2579351233@qq.com>
2025-03-14 22:15:01 +0800

    创建实验房间结构体

---
+++ lab_room.c
@@ -0,0 +1,10 @@
+#include "lab_room.h"
+
+LabRoom* CreateLabRoom()
+{
+    return nullptr;
+}
+
+void DestoryLabRoom(LabRoom* lab_room)
+{
+}
---
+++ lab_room.h
@@ -0,0 +1,19 @@
+#ifndef LAB_ROOM_H
+#define LAB_ROOM_H
+
+#include "configs.h"
+#include "linked_list.h"
+#include "experimental_equipment.h"
+
+typedef struct _LabRoom {
+    int id;                            // 房间编号
+    char name[LABROOM_LENGTH];         // 房间名称
+    LinkedList technician_id_list;     // 负责实验员ID链
+    LinkedList equipments_list;        // 所属设备id链
+} LabRoom;
+
+LabRoom* CreateLabRoom();
+
+void DestoryLabRoom(LabRoom* lab_room);
+
+#endif // !LAB_ROOM_H
