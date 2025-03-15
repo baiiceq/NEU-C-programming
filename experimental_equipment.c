2ed0c00d7b6b275394bee5a9ae90262bddf8dd06
wzyzsy <2579351233@qq.com>
2025-03-15 11:38:51 +0800

    添加设备的创建函数

--- experimental_equipment.c
+++ experimental_equipment.c
@@ -1,10 +1,27 @@
 #include "experimental_equipment.h"
 
-ExperimentalEquipment* CreateExperimentalEquipment()
+ExperimentalEquipment* CreateExperimentalEquipment(Category category, char* name, int room_id, int price, char* purchase_date)
 {
-    return nullptr;
+    ExperimentalEquipment* ee = (ExperimentalEquipment*)malloc(sizeof(ExperimentalEquipment));
+
+    if (ee == NULL)
+    {
+        printf("创建设备失败\n");
+        return NULL;
+    }
+
+    ee->id = GetNewId(EquipmentID);
+    ee->category = category;
+    strcpy_s(ee->name, EQUIPMENT_LENGTH, name);
+    ee->price = price;
+    ee->room_id = room_id;
+    strcpy_s(ee->purchase_date, DATE_LENGTH, purchase_date);
+
+    return ee;
+
 }
 
 void DestoryExperimentalEquipment(ExperimentalEquipment* experimental_equipment)
 {
+
 }
