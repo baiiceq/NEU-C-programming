68035c5b9c390111f28668b315a9d04989f7e9c0
wzyzsy <2579351233@qq.com>
2025-03-15 11:42:14 +0800

    添加了实验室的创建函数

--- lab_room.c
+++ lab_room.c
@@ -1,8 +1,21 @@
 #include "lab_room.h"
 
-LabRoom* CreateLabRoom()
+LabRoom* CreateLabRoom(char* name)
 {
-    return nullptr;
+    LabRoom* new_labroom = (LabRoom*)malloc(sizeof(LabRoom));
+
+    if (new_labroom == NULL)
+    {
+        printf("创建实验室失败\n");
+        return NULL;
+    }
+
+    strcpy_s(new_labroom->name, LABROOM_LENGTH, name);
+    new_labroom->id = GetNewId(RoomID);
+    new_labroom->equipments_list = CreateLinkedList();
+    new_labroom->technician_id_list = CreateLinkedList();
+
+    return new_labroom;
 }
 
 void DestoryLabRoom(LabRoom* lab_room)
--- lab_room.h
+++ lab_room.h
@@ -8,11 +8,11 @@
 typedef struct _LabRoom {
     int id;                            // 房间编号
     char name[LABROOM_LENGTH];         // 房间名称
-    LinkedList technician_id_list;     // 负责实验员ID链
-    LinkedList equipments_list;        // 所属设备id链
+    LinkedList* technician_id_list;     // 负责实验员ID链
+    LinkedList* equipments_list;        // 所属设备id链
 } LabRoom;
 
-LabRoom* CreateLabRoom();
+LabRoom* CreateLabRoom(char* name);
 
 void DestoryLabRoom(LabRoom* lab_room);
 
